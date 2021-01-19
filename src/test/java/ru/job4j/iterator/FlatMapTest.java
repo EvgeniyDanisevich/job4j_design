4a0797ca8920d46ec377e6cdf2d7c032a5a3f421
EvgeniyDanisevich <a937va@gmail.com>
2021-01-19 19:35:22 +0300

    checkstyle

--- src/test/java/ru/job4j/iterator/FlatMapTest.java
+++ src/test/java/ru/job4j/iterator/FlatMapTest.java
@@ -2,7 +2,9 @@ package ru.job4j.iterator;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+
 import org.junit.Test;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
