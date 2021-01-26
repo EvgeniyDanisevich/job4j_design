c666b39a9e08dc8bac1c2c891ead473db96ce980
EvgeniyDanisevich <a937va@gmail.com>
2021-01-26 18:57:07 +0300

    1. Реализовать коллекцию Set на массиве [#451586]

--- src/main/java/ru/job4j/collection/SimpleSet.java
+++ src/main/java/ru/job4j/collection/SimpleSet.java
@@ -1,62 +1,54 @@
 package ru.job4j.collection;
 
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleSet<E> implements Iterable<E> {
     private SimpleDynamicArray<E> container = new SimpleDynamicArray<>();
     private int capacity = 0;
-    private int modCount = 0;
 
-    public void add(E e) {
+    private boolean isPresent(E e) {
         for (E el : container) {
-            if (el.equals(e)) {
-                return;
+            if (Objects.equals(el, e)) {
+                return true;
             }
         }
-        container.add(e);
-        modCount++;
-        capacity++;
+        return false;
     }
 
-    public void remove(E e) {
+    private int getIndex(E e) {
         int index = 0;
         for (E el : container) {
-            if (el.equals(e)) {
-                container.remove(index);
-                modCount++;
-                break;
+            if (Objects.equals(el, e)) {
+                return index;
             }
             index++;
         }
-        capacity--;
+        return -1;
+    }
+
+    public void add(E e) {
+        if (!isPresent(e)) {
+            container.add(e);
+            capacity++;
+        }
+    }
+
+    public void remove(E e) {
+        int index;
+        for (E el : container) {
+            if (Objects.equals(el, e)) {
+                index = getIndex(e);
+                Objects.checkIndex(index, capacity);
+                container.remove(index);
+                capacity--;
+                break;
+            }
+        }
     }
 
     @Override
     public Iterator<E> iterator() {
-        return new Iterator<>() {
-            private final int expectedModCount = modCount;
-            private int iterationValue = 0;
-
-            @Override
-            public boolean hasNext() {
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return iterationValue < capacity;
-            }
-
-            @Override
-            public E next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return container.get(iterationValue++);
-            }
-        };
+        return container.iterator();
     }
 }
