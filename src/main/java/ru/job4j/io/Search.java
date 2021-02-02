f4c043c4b52566fbd884a0089fcc52fa2ee7ffc9
EvgeniyDanisevich <a937va@gmail.com>
2021-02-03 00:37:18 +0300

    5. Валидация параметров запуска. [#451608]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,8 +8,13 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+        if (args.length <= 1) {
+            throw new IllegalArgumentException(
+                    "Root folder or extension is null. Usage java -jar search.jar ROOT_FOLDER extension"
+            );
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
