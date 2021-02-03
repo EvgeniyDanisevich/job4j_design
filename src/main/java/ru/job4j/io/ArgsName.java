90fb65dd1291a4c3e90dc82bf8fa90e2795065fb
EvgeniyDanisevich <a937va@gmail.com>
2021-02-04 00:06:02 +0300

    5.1. Именованные аргументы [#451613]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -7,16 +7,21 @@ public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
 
     public String get(String key) {
-        if (!values.containsKey(key)) {
-            throw new IllegalArgumentException();
-        }
         return values.get(key);
     }
 
     private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException();
+        }
         Arrays.stream(args)
                 .map(s -> s.split("="))
-                .filter(strings -> strings.length == 2)
+                .filter(strings -> {
+                    if (strings.length != 2) {
+                        throw new IllegalArgumentException();
+                    }
+                    return true;
+                })
                 .forEach(strings -> values.put(strings[0].substring(1), strings[1]));
     }
 
