2f9134cab10c34aeb1393feba518743f4756e33c
EvgeniyDanisevich <a937va@gmail.com>
2021-02-06 17:37:48 +0300

    2. Анализ доступности сервера. [#451616]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -6,33 +6,31 @@ import java.util.*;
 public class Analizy {
     public void unavailable(String source, String target) {
         List<String> filteredLines = new ArrayList<>();
-        List<String> dates = new ArrayList<>();
-        List<String> datesFormat = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             boolean flag = false;
             String str;
+            String begin = null;
+            String end = null;
             while ((str = in.readLine()) != null) {
                 if ((str.startsWith("400") || str.startsWith("500")) && !flag) {
-                    filteredLines.add(str);
+                    begin = str.substring(4);
                     flag = true;
                 }
-                if ((str.startsWith("200") || str.startsWith("300"))
-                        && (flag || !filteredLines.isEmpty())) {
-                    filteredLines.add(str);
+                if ((str.startsWith("200") || str.startsWith("300")) && (flag)) {
+                    end = str.substring(4);
                     flag = false;
                 }
+                if (begin != null && end != null) {
+                    filteredLines.add(String.format("%s;%s", begin, end));
+                    begin = null;
+                    end = null;
+                }
             }
-            filteredLines.stream()
-                    .map(s -> s.split(" "))
-                    .forEach(strings -> dates.add(strings[1]));
         } catch (Exception e) {
             e.printStackTrace();
         }
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            for (int i = 0; i < dates.size(); i = i + 2) {
-                datesFormat.add(dates.get(i) + ";" + dates.get(i + 1));
-            }
-            datesFormat.forEach(out::println);
+            filteredLines.forEach(out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
